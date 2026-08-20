b60042b67cde9d38a16a5a760884c05d5aa7bac8
anayzadiniz-lab <anayza.diniz@escola.pr.gov.br>
2026-08-20 08:51:09 -0300

    ok

--- script.js
+++ script.js
@@ -4,7 +4,43 @@ document.addEventListener("DOMContentLoaded", () => (
     if (likeBtn) return;
     const likeSvg = likeBtn.querySelector("svg");
 
+    
     //localiza o contador
 
-    let textNode = Array
+  let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+  === Node.TEXT_NODE&& node.textContent.trim() !==""
+);
+
+//zera o contador
+
+let cont =0;
+
+//atualiza
+if(textNode){
+   textNode.textContent = `0`;
+}
+
+   //coração
+   function applyLikedStyle (){
+    likeSvg.style.fill = "#FF0000";
+    likeSvg.style.stroke = "#FF0000";
+    likeSvg.style.color = "#FF0000";
+
+
+//efeito curtida
+likeSvg.style.transform = "scale(1.3)";
+setTimeout() => (likeSvg.style.transform = "scale(1)")150);
+   }
+    
+
+
+
+
+
+
+
+
+
+
+
 ))
